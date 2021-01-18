ba1de03ccf48740ac5238af332a3d8d0e0abf61d
Thinkin <thinkin@live.com>
2021-01-18 16:54:37 +0800

    first relaese

--- pyqt_1/main.py
+++ pyqt_1/main.py
@@ -1,16 +1,32 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5.QtWidgets import QWidget, QMainWindow, QApplication
+from PyQt5.QtGui import QFont
+import sys
+from untitled import Ui_MainWindow
 
 
-# Press the green button in the gutter to run the script.
+class Myapp(QMainWindow):
+    def __init__(self):
+        super(Myapp, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.spinBox_quantity.valueChanged.connect(self.setSpinboxValue)
+        self.ui.spinBox_price.valueChanged.connect(self.setSpinboxValue)
+
+    def on_pushButton_clicked(self):
+        quantity = int(self.ui.lineEdit_quantity.text())
+        price = float(self.ui.lineEdit_price.text())
+        self.ui.lineEdit_sum.setText('{:.6f}'.format(quantity * price))
+
+    def setSpinboxValue(self):
+        quantity = self.ui.spinBox_quantity.value()
+        price = self.ui.spinBox_price.value()
+        self.ui.spinBox_sum.setValue(quantity * price)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    demoapp = Myapp()
+    demoapp.show()
+    sys.exit(app.exec_())
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
