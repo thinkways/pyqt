255724b79508e67d899c5cc2a54fae95ede58811
Thinkin <thinkin@live.com>
2021-01-19 11:02:07 +0800

    ui class generated

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QFont
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+class MyApp(QMainWindow):
+    def __init__(self):
+        super(MyApp, self).__init__()
