383e62c07960299381c54c5af12fbe338a84597c
thinkin <thinkin@live.com>
2021-01-25 19:14:21 +0800

    mac modified

--- pythonProject3/main.py
+++ pythonProject3/main.py
@@ -1,3 +1,17 @@
 from PyQt5.QtWidgets import QWidget, QApplication
 import sys
+from untitled import Ui_Form
 
+
+class MyApp(QWidget):
+    def __init__(self):
+        super(MyApp, self).__init__()
+        self.ui = Ui_Form()
+        self.ui.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    demoApp = MyApp()
+    demoApp.show()
+    sys.exit(app.exec_())
